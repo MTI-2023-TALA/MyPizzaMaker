3583785dbf3e08420f7c82235a09d46adcd93ba1
Théo Casner <theo.casner@epita.fr>
2022-04-27 17:28:42 +0200

    add(db): added sql files

---
+++ PizzaMakerDatabase/dbo/Tables/ingredients.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ingredients] (
+    [id]           INT           IDENTITY (1, 1) NOT NULL,
+    [name]         VARCHAR (50)  NOT NULL,
+    [image_path]   VARCHAR (250) NULL,
+    [is_available] BIT           NOT NULL,
+    [category]     VARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_ingredients] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ PizzaMakerDatabase/dbo/Tables/pizzas_ingredients.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[pizzas_ingredients] (
+    [ingredient_id] INT NOT NULL,
+    [pizza_id]      INT NOT NULL,
+    CONSTRAINT [FK_pizzas_ingredients_ingredients] FOREIGN KEY ([ingredient_id]) REFERENCES [dbo].[ingredients] ([id]),
+    CONSTRAINT [FK_pizzas_ingredients_pizzas] FOREIGN KEY ([pizza_id]) REFERENCES [dbo].[pizzas] ([id])
+);
+
