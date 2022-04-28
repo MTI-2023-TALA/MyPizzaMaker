45c186557fc4927052bdd24fab5a2691e25602d0
Théo Casner <theo.casner@epita.fr>
2022-04-28 12:02:30 +0200

    add(repository): pizza repository added 2 get

--- backend/DataAccess/Interfaces/IPizzaRepository.cs
+++ backend/DataAccess/Interfaces/IPizzaRepository.cs
@@ -2,5 +2,7 @@
 {
     public interface IPizzaRepository : DataAccess.IRepository<EfModels.Pizza, Dto.Pizza>
     {
+        public List<Dto.Pizza> GetAllPizzas();
+        public Dto.Pizza GetPizza(long pizzaId);
     }
 }
--- backend/DataAccess/PizzaRepository.cs
+++ backend/DataAccess/PizzaRepository.cs
@@ -9,5 +9,17 @@ namespace backend.DataAccess
         public PizzaRepository(myPizzaMakerContext context, ILogger logger, IMapper mapper) : base(context, logger, mapper)
         {
         }
+
+        public List<Dto.Pizza> GetAllPizzas()
+        {
+            var result = _context.Pizzas.ToList();
+            return _mapper.Map<List<Dto.Pizza>>(result);
+        }
+
+        public Dto.Pizza GetPizza(long pizzaId)
+        {
+            var result = _context.Pizzas.Where(p => p.Id == pizzaId);
+            return _mapper.Map<Dto.Pizza>(result);
+        }
     }
 }
