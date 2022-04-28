bae34be05a34941e75089685af1c65cc737880fd
Alois Heloir <alois.heloir@live.fr>
2022-04-28 19:33:20 +0200

    feat: Add 2 app: backoffice and frontoffice

---
+++ FrontOffice/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontOffice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
