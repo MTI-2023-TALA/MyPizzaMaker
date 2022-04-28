93d6ab9ca10ec0bdeb1796fd8e510cde11644e1f
Alois Heloir <alois.heloir@live.fr>
2022-04-28 15:16:34 +0200

    feat: Add logger

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,14 +1,23 @@
-using AutoMapper; 
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<backend.DataAccess.EfModels.myPizzaMakerContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myPizzaMaker"));
+});
+builder.Services.AddLogging();
+builder.Services.AddSingleton(typeof(ILogger), typeof(Logger<backend.Logger>));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddAutoMapper(typeof(backend.DataAccess.AutomapperProfiles));
+
 builder.Services.AddTransient<backend.DataAccess.Interfaces.ICartRepository, backend.DataAccess.CartRepository>();
 builder.Services.AddTransient<backend.DataAccess.Interfaces.IIngredientRepository, backend.DataAccess.IngredientRepository>();
 builder.Services.AddTransient<backend.DataAccess.Interfaces.IPizzaRepository, backend.DataAccess.PizzaRepository>();
