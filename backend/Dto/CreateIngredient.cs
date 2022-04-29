ae6301bc683c8fc328dd0a4968e9906f132e293e
Théo Casner <theo.casner@epita.fr>
2022-04-29 12:07:12 +0200

    add(fix): fix typos

--- backend/Dto/CreateIngredient.cs
+++ backend/Dto/CreateIngredient.cs
@@ -3,7 +3,7 @@
     public class CreateIngredient
     {
         public string Name { get; set; }
-        public bool IsAvalaible { get; set; }
+        public bool IsAvailable { get; set; }
         public string Category { get; set; }
     }
 }
--- backend/Dto/UpdateIngredient.cs
+++ backend/Dto/UpdateIngredient.cs
@@ -3,7 +3,7 @@
     public class UpdateIngredient
     {
         public string Name { get; set; }
-        public bool IsAvalaible { get; set; }
+        public bool IsAvailable { get; set; }
         public string Category { get; set; }
     }
 }
