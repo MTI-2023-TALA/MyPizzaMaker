18f3d0b20ab409cd08e0af367bde57fb55568026
Théo Casner <theo.casner@epita.fr>
2022-05-02 11:54:17 +0200

    add(dbo, dto): added  dbo and dto for ingredient stats

---
+++ backend/Dbo/IngredientStats.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Dbo
+{
+    public class IngredientStats
+    {
+        public String Name { get; set; }
+        public int Count { get; set; }
+
+        public IngredientStats()
+        {
+
+        }
+
+        public IngredientStats(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+}
