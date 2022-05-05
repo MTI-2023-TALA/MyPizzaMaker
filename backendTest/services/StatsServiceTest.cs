22a78cc7a4101da35d9d9c916a8743a19da593af
Théo Casner <theo.casner@epita.fr>
2022-05-05 16:13:23 +0200

    add(stats service test): end of tests for Stats Service

--- backendTest/services/StatsServiceTest.cs
+++ backendTest/services/StatsServiceTest.cs
@@ -63,13 +63,8 @@ namespace backendTest.services
             context.Dispose();
         }
 
-        private async void PopulateDB()
-        {
-
-        }
-
         [Fact]
-        public async void GetDailyStats()
+        public async void TestGetDailyStats()
         {
             DateTime now = DateTime.Now;
             DateTime dateTime = now.AddHours(-1);
@@ -91,7 +86,7 @@ namespace backendTest.services
         }
 
         [Fact]
-        public async void GetWeeklyStats()
+        public async void TestGetWeeklyStats()
         {
             DateTime today = DateTime.Today;
             DateTime dateTime1 = today.AddDays(-3);
@@ -119,7 +114,7 @@ namespace backendTest.services
         }
 
         [Fact]
-        public async void GetMonthlyStats()
+        public async void TestGetMonthlyStats()
         {
             DateTime now = DateTime.Now;
             DateTime dateTime1 = new DateTime(
@@ -152,6 +147,61 @@ namespace backendTest.services
 
             int monthlyStats = await _statsService.GetMonthlyStats();
             Assert.Equal(2, monthlyStats);
+        }
+
+        [Fact]
+        public async void TestGetIngredientsStats()
+        {
+            //------- TEST SETUP
+            // create carts
+            await _cartService.CreateCart(new backend.Dto.CreateCart
+            {
+                Status = "in creation",
+                Date = new DateTime(2022, 12, 20),
+            });
+            await _cartService.CreateCart(new backend.Dto.CreateCart
+            {
+                Status = "served",
+                Date = new DateTime(2022, 12, 22),
+            });
+
+            // create ingredients
+            await _ingredientService.CreateIngredient(new backend.Dto.CreateIngredient
+            {
+                Name = "Dough",
+                IsAvailable = true,
+                Category = "dough",
+            });
+            await _ingredientService.CreateIngredient(new backend.Dto.CreateIngredient
+            {
+                Name = "Cheese",
+                IsAvailable = true,
+                Category = "cheese",
+            });
+
+            // create Dtos to send
+            backend.Dto.AddPizza addPizzaDto = new backend.Dto.AddPizza
+            {
+                Name = "Pizza 1",
+                IngredientIds = new List<int> { 1, 2 },
+            };
+            backend.Dto.AddPizza addPizza2Dto = new backend.Dto.AddPizza
+            {
+                Name = "Pizza 2",
+                IngredientIds = new List<int> { 1 },
+            };
+
+            // add Pizzas to db
+            await _cartService.AddPizzaToCart(1, addPizzaDto);
+            await _cartService.AddPizzaToCart(2, addPizza2Dto);
+
+            //------- Test Check
+            List<backend.Dto.IngredientStats> ingredientStats = await _statsService.GetIngredientsStats();
+            Assert.Equal(2, ingredientStats.Count());
+            Assert.Equal("Dough", ingredientStats[0].Name);
+            Assert.Equal(2, ingredientStats[0].Count);
+            Assert.Equal("Cheese", ingredientStats[1].Name);
+            Assert.Equal(1, ingredientStats[1].Count);
         }
     }
 }
