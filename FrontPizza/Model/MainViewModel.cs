e46df435bc84726254d9fb35a1277eefd27061c0
Alan Schaeffer <alan.schaeffer29@gmail.com>
2022-05-06 19:36:01 +0200

    Added categories to ingredient

--- FrontPizza/Model/MainViewModel.cs
+++ FrontPizza/Model/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MvvmHelpers;
+﻿using backend.Dto;
+using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,19 +13,65 @@ namespace FrontPizza.Model
         private IngredientService _ingredientService;
         private CartService _cartService;
 
+        public ObservableRangeCollection<Ingredient> DoughIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> BaseIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> CheeseIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> SauceIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> MeatIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> AccompanimentsIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> DrinkIngredients { get; set; }
+        public ObservableRangeCollection<Ingredient> DessertIngredients { get; set; }
+
         public int Id { get; set; }
-        public ObservableRangeCollection<backend.Dto.Ingredient> Ingredients { get; set; }
 
         public async Task LoadIngredients()
         {
-            Ingredients.Clear();
+            DoughIngredients.Clear();
+            BaseIngredients.Clear();
+            CheeseIngredients.Clear();
+            SauceIngredients.Clear();
+            MeatIngredients.Clear();
+            AccompanimentsIngredients.Clear();
+            DrinkIngredients.Clear();
+            DessertIngredients.Clear();
 
-            List<backend.Dto.Ingredient> ingredients = await _ingredientService.LoadIngredients();
+            List<Ingredient> ingredients = await _ingredientService.LoadIngredients();
             foreach (var ingredient in ingredients)
             {
                 if (ingredient.IsAvailable)
                 {
-                    Ingredients.Add(ingredient);
+                    if (ingredient.Category == "dough")
+                    {
+                        DoughIngredients.Add(ingredient);
+                    }
+                    if (ingredient.Category == "base")
+                    {
+                        BaseIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "cheese")
+                    {
+                        CheeseIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "sauce")
+                    {
+                        SauceIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "meat")
+                    {
+                        MeatIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "accompaniments")
+                    {
+                        AccompanimentsIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "drink")
+                    {
+                        DrinkIngredients.Add(ingredient);
+                    }
+                    else if (ingredient.Category == "dessert")
+                    {
+                        DessertIngredients.Add(ingredient);
+                    }
                 }
             }
         }
@@ -54,8 +101,15 @@ namespace FrontPizza.Model
         {
             _ingredientService = new IngredientService();
             _cartService = new CartService();
+            DoughIngredients = new ObservableRangeCollection<Ingredient>();
+            BaseIngredients = new ObservableRangeCollection<Ingredient>();
+            SauceIngredients = new ObservableRangeCollection<Ingredient>();
+            CheeseIngredients = new ObservableRangeCollection<Ingredient>();
+            MeatIngredients = new ObservableRangeCollection<Ingredient>();
+            AccompanimentsIngredients = new ObservableRangeCollection<Ingredient>();
+            DrinkIngredients = new ObservableRangeCollection<Ingredient>();
+            DessertIngredients = new ObservableRangeCollection<Ingredient>();
             this.Id = Id;
-            this.Ingredients = new ObservableRangeCollection<backend.Dto.Ingredient>();
         }
     }
 }
